d0f39bd039b021c764a3d5a32fc0462508d9ff83
Joel Ambass <joel@electricfeel.com>
2020-08-31 17:19:20 +0200

    add os_mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule SpotifyWall.MixProject do
   def application do
     [
       mod: {SpotifyWall.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
