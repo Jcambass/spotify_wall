149a860d1816ac75901c4b4db6b73f776e67c965
Joel Ambass <joel@electricfeel.com>
2020-09-01 16:46:28 +0200

    some design

--- lib/spotify_wall_web/live/audio_tile_component.ex
+++ lib/spotify_wall_web/live/audio_tile_component.ex
@@ -3,13 +3,13 @@ defmodule SpotifyWallWeb.AudioTileComponent do
 
   def render(assigns) do
     ~L"""
-      <li id="<%= @id %>" class="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+      <li id="<%= @id %>" class="py-10 px-6 bg-gray-500 text-center rounded-lg xl:px-10 xl:text-left shadow-lg">
         <div class="space-y-6 xl:space-y-10">
           <p class="font-medium text-lg text-indigo-200 text-center"><%= @nickname %> listens to</p>
           <%= if @activity.preview do %>
             <audio id="audio-<%= @id %>" src="<%= @activity.preview %>"></audio>
           <% end %>
-          <img onclick="togglePlay('audio-<%= @id %>')" class="mx-auto h-40 w-40 rounded-lg xl:w-56 xl:h-56" src="<%= @activity.image %>" alt="">
+          <img onclick="togglePlay('audio-<%= @id %>')" class="shadow-lg mx-auto h-40 w-40 rounded-lg xl:w-56 xl:h-56" src="<%= @activity.image %>" alt="">
           <div class="space-y-2 xl:flex xl:items-center xl:justify-between">
             <div class="font-medium text-lg leading-6 space-y-1">
               <a href="<%= @activity.url %>">
