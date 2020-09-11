1c1c5ece71040bba7c702982e0174e3fad71edd9
Joel Ambass <joel@electricfeel.com>
2020-09-11 12:28:28 +0200

    add confirmation for remove member

--- lib/spotify_wall_web/views/wall_view.ex
+++ lib/spotify_wall_web/views/wall_view.ex
@@ -1,16 +1,21 @@
 defmodule SpotifyWallWeb.WallView do
   use SpotifyWallWeb, :view
+  alias SpotifyWall.Invitations.Invitation
+  alias SpotifyWall.Memberships.Membership
 
   def is_owner?(wall, user) do
     wall.owner.id == user.id
   end
 
-  def open_revoke_invite_var(invite) do
-    "open_revoke_invite_#{invite.id}"
+  def open_modal_var(struct) do
+    case struct do
+      %Invitation{id: id} -> "open_revoke_invite_#{id}"
+      %Membership{id: id} -> "open_remove_member_#{id}"
+    end
   end
 
-  def init_open_revoke_invite_vars(invites) do
-    Enum.map(invites, fn i -> "#{open_revoke_invite_var(i)}: false" end)
+  def init_open_modal_vars(structs) do
+    Enum.map(structs, fn struct -> "#{open_modal_var(struct)}: false" end)
     |> Enum.join(", ")
   end
 end
