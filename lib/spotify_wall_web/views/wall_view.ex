fe409a92cc36d3f00c1fcc383d0141e62fd960ef
Joel Ambass <joel@electricfeel.com>
2020-09-11 12:08:53 +0200

    confirm invitation revoke

--- lib/spotify_wall_web/views/wall_view.ex
+++ lib/spotify_wall_web/views/wall_view.ex
@@ -4,4 +4,13 @@ defmodule SpotifyWallWeb.WallView do
   def is_owner?(wall, user) do
     wall.owner.id == user.id
   end
+
+  def open_revoke_invite_var(invite) do
+    "open_revoke_invite_#{invite.id}"
+  end
+
+  def init_open_revoke_invite_vars(invites) do
+    Enum.map(invites, fn i -> "#{open_revoke_invite_var(i)}: false" end)
+    |> Enum.join(", ")
+  end
 end
